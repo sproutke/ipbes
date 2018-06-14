6cff592f3c0420571fd609308186f73620088f39
Trevor Kimenye <kimenye@gmail.com>
2018-06-14 11:43:35 +0300

    reworked news and footer

--- panels/layouts/ipbes_front_new/panels-ipbes-front-new.tpl.php
+++ panels/layouts/ipbes_front_new/panels-ipbes-front-new.tpl.php
@@ -64,11 +64,31 @@
       </div>
     </div>
   <?php endif; ?>
-
-  <?php if($content['footer']): ?>
-    <footer class="footer">
-      <?php print $content['footer']; ?>
-    </footer>
-  <?php endif; ?>
-
 </div>
+
+<footer id="footer" class="footer">
+  <div class="container copyright">
+    <div class="row">
+      <div class="ipbes-mark col-md-6">
+        <p>&copy; IPBES Secretariat</p>
+        <?php if($content['footer-contacts']): ?>
+          <?php print $content['footer-contacts']; ?>
+        <?php endif; ?>
+      </div>
+      <div class="partner-logos col-md-6">
+        <a href="//www.unep.org" target="_blank">
+          <img src="/sites/all/themes/ipbes_new/panels/layouts/ipbes_front_new/img/unep.png" alt="UNEP"/>
+        </a>
+        <a href="//en.unesco.org" target="_blank">
+          <img src="/sites/all/themes/ipbes_new/panels/layouts/ipbes_front_new/img/unesco.png" alt="UNESCO"/>
+        </a>
+        <a href="//www.fao.org/home/en" target="_blank">
+          <img src="/sites/all/themes/ipbes_new/panels/layouts/ipbes_front_new/img/fao.png" alt="FAO"/>
+        </a>
+        <a href="//www.undp.org" class="slim" target="_blank">
+          <img src="/sites/all/themes/ipbes_new/panels/layouts/ipbes_front_new/img/undp.png" alt="UNDP"/>
+        </a>
+      </div>
+    </div>
+  </div>
+</footer>
