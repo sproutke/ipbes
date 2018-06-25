cd952ffd5c831996762fddaf2bb192066474266f
Trevor Kimenye <kimenye@gmail.com>
2018-06-25 16:45:09 +0300

    Added js for placeholder

--- js/ipbes_new.script.js
+++ js/ipbes_new.script.js
@@ -4,5 +4,8 @@
  */
 (function ($) {
   // Add your code here.
-  console.log('Testing of custom scripts - jqueryWorks')
+  // console.log('Testing of custom scripts - jqueryWorks')
+
+  // temporary adding of placeholder
+  jQuery('#views-exposed-form-search-index-page input[type="text"]').attr("placeholder", "Search by keyword or title");
 })(jQuery);
